04f04626c54e24153ca7722ed64b89bb7b28096a
Jachu05 <jachu05@gmail.com>
2022-06-20 00:56:52 +0200

    16_03_tut

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,13 +1,24 @@
-from fastapi.testclient import TestClient
+from app import schemas
+# noinspection PyUnresolvedReferences
+from app.models import User
+from .database import client, session
 
-from app.main import app
 
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client, session):
     res = client.get("/")
 
 
-def test_create_user():
-    pass
+def test_create_user(client, session):
+    res = client.post("/users/", json={"email": "hello123@xd.pl", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@xd.pl"
+    assert res.status_code == 201
+
+
+def test_login_user(client, session):
+    users = session.query(User).all()
+    print(users)
+    res = client.post("/login", data={"username": "hello123@xd.pl", "password": "password123"})
+
+    assert res.status_code == 200
